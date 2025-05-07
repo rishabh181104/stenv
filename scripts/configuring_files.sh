0df82a931be278a7eb52e7aba52c777fe1ad8c4d
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-05-08 00:23:40 +0530

    s

--- scripts/configuring_files.sh
+++ scripts/configuring_files.sh
@@ -3,12 +3,16 @@
 # Base paths
 SOURCE_BASE="$HOME/stenv"
 TARGET_BASE="$HOME/.config"
+HOME_BASE="$HOME"
 
 # List of directories to replace
 DIRS_TO_REPLACE=("fastfetch" "foot" "kitty" "nvim" "wallust" "wlogout" "fish" "hypr" "mako" "rofi" "waybar" "i3" "i3status" "picom")
 
 # List of files to replace
-FILES_TO_REPLACE=("starship.toml" ".xinitrc")
+FILES_TO_REPLACE=("starship.toml")
+
+# List of files to replace in the home dir
+FILES_TO_REPLACE_IN_HOME_DIR=(".xinitrc")
 
 # === Replace Directories ===
 echo "🔁 Replacing Directories..."
@@ -48,6 +52,27 @@ for FILE_NAME in "${FILES_TO_REPLACE[@]}"; do
 
     echo "📄 Copying $SRC to $TARGET_BASE"
     ln -s "$SRC" "$TARGET_BASE"
+
+done
+
+# ===Replacing files in home folder===
+echo "🔁 Replacing Files..."
+for FILE_NAME in "${FILES_TO_REPLACE_IN_HOME_DIR[@]}"; do
+    SRC="$SOURCE_BASE/$FILE_NAME"
+    HOM="$HOME_BASE/$FILE_NAME"
+
+    if [ ! -f "$SRC" ]; then
+        echo "❌ Source file does not exist: $SRC"
+        continue
+    fi
+
+	if [ -f "$HOM" ]; then
+		echo "🗑️ Removing existing file: $HOM"
+		rm -f "$HOM"
+	fi
+
+	echo "📄 Copying $SRC to $HOME_BASE"
+	ln -s "$SRC" "$HOME_BASE"
 done
 
 echo "✅ All done."
