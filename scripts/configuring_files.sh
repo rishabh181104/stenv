ccbd11659f7270e9437de2312931513fbcff076a
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-05-20 20:35:01 +0530

    just some changes to waybar

--- scripts/configuring_files.sh
+++ scripts/configuring_files.sh
@@ -6,7 +6,7 @@ TARGET_BASE="$HOME/.config"
 HOME_BASE="$HOME"
 
 # List of directories to replace
-DIRS_TO_REPLACE=("ghostty" "wal" "fastfetch" "foot" "kitty" "nvim" "wlogout" "fish" "hypr" "mako" "rofi" "waybar" "i3" "i3status" "picom" "qutebrowser" "dunst")
+DIRS_TO_REPLACE=("ghostty" "wal" "fastfetch" "foot" "kitty" "nvim" "wlogout" "fish" "hypr" "mako" "rofi" "waybar" "i3" "i3status" "picom" "dunst")
 
 # List of files to replace
 FILES_TO_REPLACE=("starship.toml")
